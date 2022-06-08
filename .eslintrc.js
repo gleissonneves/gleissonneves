27d5f8b532a6bf0082a8a859b520735f1b42358b
Gleisson Neves <gleissonneves2017@gmail.com>
2022-06-08 12:39:27 -0300

    feat: adicionado favicon

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 'off',
-    "skipBlankLines": false
   },
 };
